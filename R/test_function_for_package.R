cb647844fd1a3016b29cfe054e4653bdfe1ab9c6
John-o Kulas <kulasj@montclair.edu>
2022-02-07 14:02:23 -0500

    Small tweaks

--- R/test_function_for_package.R
+++ R/test_function_for_package.R
@@ -9,22 +9,22 @@ ahat<-function(x){
 }#FUNCTION TO ESTIMATE THE CTT-A STATISTIC, WHICH IS THE EQUIVALENT TO THE DISCRIMINATION STATISTIC IN IRT
 
 
-alphas<-psych::alpha(data, check.keys = TRUE)#COMPUTING ALPHAS FOR ALL 100 ITEMS. WE NEED THIS IN ORDER TO GET THE CORRECTED ITEM-TOTAL CORRELATIONS, WHICH WE THEN USE FOR COMPUTING THE CTT-A STATISTIC.
-citcs<-data.frame(alphas$item.stats$r.drop)#ACCESSING THE CORRECTED ITEM-TOTAL CORRELATIONS INSIDE alphas.
-pseudoA<-data.frame(ahat(citcs))#USING THE ahat FUNCTION TO CALCULATE THE CTT-A PARAMETER FOR ALL 100 ITEMS. CORRECTED ITEM-TOTAL CORRELATION ARE ENTERED AS AN ARGUMENT.
-pseudoB.temp<-data.frame(qnorm(colMeans(data, na.rm=TRUE)))#CALCULATING THE CTT-B PARAMETER, WHICH IS JUST THE PROBABILITIES OF ANSWERING RIGHT FOR EACH ITEM.
-pseudoB<- -2*pseudoB.temp
-df<-as.data.frame(cbind(citcs, pseudoA, pseudoB))#PUTTING ALL RELEVANT STATISTIC TOGETHER
+alphas<-psych::alpha(data, check.keys = TRUE)                #COMPUTING ALPHAS FOR ALL 100 ITEMS. WE NEED THIS IN ORDER TO GET THE CORRECTED ITEM-TOTAL CORRELATIONS, WHICH WE THEN USE FOR COMPUTING THE CTT-A STATISTIC.
+citcs<-data.frame(alphas$item.stats$r.drop)                  #ACCESSING THE CORRECTED ITEM-TOTAL CORRELATIONS INSIDE alphas.
+pseudoA<-data.frame(ahat(citcs))                             #USING THE ahat FUNCTION TO CALCULATE THE CTT-A PARAMETER FOR ALL 100 ITEMS. CORRECTED ITEM-TOTAL CORRELATION ARE ENTERED AS AN ARGUMENT.
+pseudoB.temp<-data.frame(qnorm(colMeans(data, na.rm=TRUE)))  #CALCULATING THE CTT-B PARAMETER, WHICH IS JUST THE PROBABILITIES OF ANSWERING RIGHT FOR EACH ITEM.
+pseudoB<- -2*pseudoB.temp                                    ## from simulations (b ~ z_g; normal ability distribution)
+df<-as.data.frame(cbind(citcs, pseudoA, pseudoB))            #PUTTING ALL RELEVANT STATISTIC TOGETHER
 
-colnames(df)<-c("CITC", "PseudoA", "PseudoB")#RENAMING COLUMN HEADERS
+colnames(df)<-c("CITC", "PseudoA", "PseudoB")                #RENAMING COLUMN HEADERS
 c<-0
 pseudob<-df$PseudoB[item]
 pseudoa<-df$PseudoA[item]
-# pseudob
-# pseudoa
-eq <- function(x){c + ((1-c)*(1/(1+2.71828^(-1.7*(pseudoa*(x-pseudob))))))}#FUNCTION THAT CREATES ICC BASED ON pseudob AND pseudoa
 
-curve(eq, col="red", xlim=c(-4,4), ylim=c(0,1), main="Item Characteristic Curve")#PLOTTING CTT-ICC AND IRT-ICC SIDE BY SIDE.
+
+eq <- function(x){c + ((1-c)*(1/(1+2.71828^(-1.7*(pseudoa*(x-pseudob))))))}          #FUNCTION THAT CREATES ICC BASED ON pseudob AND pseudoa
+
+curve(eq, col="red", xlim=c(-4,4), ylim=c(0,1), main="Item Characteristic Curve")    #PLOTTING CTT-ICC AND IRT-ICC SIDE BY SIDE.
 output<-cbind(pseudob, pseudoa)
 return(output)
 }
@@ -35,5 +35,17 @@ return(output)
 # icc(data,2)
 # library(cttv)
 
-icc(testdata)
+data <- read.csv("testdata.csv")
+write.csv(icc(data), "C:\\Kulas\\pseudos.csv")
+
+
+library(mirt)
+
+mod <- mirt(data, 1, itemtype="2PL")
+write.csv(coef(mod, IRTpars = TRUE, simplify = TRUE), "C:\\Kulas\\irts.csv")
+
+
+IRT_parms <- coef(mod, IRTpars = TRUE, simplify = TRUE)
+irt <- as.data.frame(IRT_parms$items)
+
 
